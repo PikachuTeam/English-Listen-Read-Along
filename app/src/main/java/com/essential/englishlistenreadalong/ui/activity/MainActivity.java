14091e6479f74cb137df961dabe594c95e727178
hoabackz <hoaback@gmail.com>
2016-02-28 00:06:10 +0700

    update component

--- app/src/main/java/com/essential/englishlistenreadalong/ui/activity/MainActivity.java
+++ app/src/main/java/com/essential/englishlistenreadalong/ui/activity/MainActivity.java
@@ -4,26 +4,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.support.v4.content.LocalBroadcastManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.essential.englishlistenreadalong.R;
 import com.essential.englishlistenreadalong.TestFragment;
 import com.essential.englishlistenreadalong.app.BaseMenuActivity;
 import com.essential.englishlistenreadalong.app.EssentialPlayer;
-import com.essential.englishlistenreadalong.app.PlayerChangeListener;
+import com.essential.englishlistenreadalong.ui.component.FullPlayerComponent;
 import com.essential.englishlistenreadalong.ui.component.SmallPlayerComponent;
 
-import tatteam.com.app_common.AppCommon;
 import tatteam.com.app_common.ui.fragment.BaseFragment;
 
-public class MainActivity extends BaseMenuActivity implements View.OnClickListener {
+public class MainActivity extends BaseMenuActivity {
     private TelephonyManager telephonyManager;
     private PhoneStateListener phoneStateListener;
     public SmallPlayerComponent smallPlayer;
+    public FullPlayerComponent fullPlayer;
     public EssentialPlayer playerController;
     public final String NEXT_PREVIOUS = "next_previous";
     public final String PLAY_PAUSE = "play_pause";
@@ -35,8 +35,12 @@ public class MainActivity extends BaseMenuActivity implements View.OnClickListen
     protected void onCreateContentView() {
         super.onCreateContentView();
         resigterBroadCast();
-        smallPlayer = new SmallPlayerComponent(MainActivity.this);
         playerController = new EssentialPlayer(this);
+        smallPlayer = new SmallPlayerComponent(MainActivity.this);
+        fullPlayer = new FullPlayerComponent(MainActivity.this);
+        playerController.player = MediaPlayer.create(MainActivity.this, R.raw.traitimbenle);
+
+        setLockMenu(true);
     }
 
     public void setupTelephony() {
@@ -61,19 +65,16 @@ public class MainActivity extends BaseMenuActivity implements View.OnClickListen
         }
     }
 
-    private void setPlayerListener(PlayerChangeListener listener) {
-        playerController.setPlayerChangeListenner(listener);
-    }
 
     private void resigterBroadCast() {
         LocalBroadcastManager.getInstance(this).registerReceiver(playPauseReceiver,
                 new IntentFilter(PLAY_PAUSE));
-        LocalBroadcastManager.getInstance(this).registerReceiver(nextPreviousReceiver,
-                new IntentFilter(NEXT_PREVIOUS));
-        LocalBroadcastManager.getInstance(this).registerReceiver(seekReceiver,
-                new IntentFilter(SEEK));
-        LocalBroadcastManager.getInstance(this).registerReceiver(repeatShuffleReceive,
-                new IntentFilter(REPEAT_SHUFFLE));
+//        LocalBroadcastManager.getInstance(this).registerReceiver(nextPreviousReceiver,
+//                new IntentFilter(NEXT_PREVIOUS));
+//        LocalBroadcastManager.getInstance(this).registerReceiver(seekReceiver,
+//                new IntentFilter(SEEK));
+//        LocalBroadcastManager.getInstance(this).registerReceiver(repeatShuffleReceive,
+//                new IntentFilter(REPEAT_SHUFFLE));
     }
 
     private BroadcastReceiver playPauseReceiver = new BroadcastReceiver() {
@@ -81,29 +82,13 @@ public class MainActivity extends BaseMenuActivity implements View.OnClickListen
         public void onReceive(Context context, Intent intent) {
             // Get extra data included in the Intent
             // String message = intent.getStringExtra("message");
-        }
-    };
-    private BroadcastReceiver nextPreviousReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            // String message = intent.getStringExtra("message");
-        }
-    };
-
-    private BroadcastReceiver seekReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            // String message = intent.getStringExtra("message");
-        }
-    };
-
-    private BroadcastReceiver repeatShuffleReceive = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            // String message = intent.getStringExtra("message");
+            String message = intent.getStringExtra("message");
+            playerController.addPlayerChangeListenner(smallPlayer);
+            playerController.addPlayerChangeListenner(fullPlayer);
+            if (playerController.player.isPlaying())
+                playerController.pause();
+            else
+                playerController.resume();
         }
     };
 
@@ -154,7 +139,6 @@ public class MainActivity extends BaseMenuActivity implements View.OnClickListen
             case R.id.history:
                 break;
             case R.id.more_app:
-                AppCommon.getInstance().openMoreAppDialog(MainActivity.this);
                 break;
         }
 
@@ -164,23 +148,9 @@ public class MainActivity extends BaseMenuActivity implements View.OnClickListen
     }
 
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        switch (id) {
-            case R.id.btn_play_small_player:
-                break;
-            case R.id.btn_next_small_player:
-                break;
-            case R.id.btn_previous_small_player:
-                break;
-            case R.id.view_player_small:
-                break;
-        }
-    }
-
-    @Override
     protected void onDestroy() {
         playerController.stop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(playPauseReceiver);
         super.onDestroy();
     }
 }
